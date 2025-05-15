3a42e5fd5f1e285c38ef758393573314e8d1db2a
stivenBermeo <bermeopalomares3@gmail.com>
2025-05-15 12:03:34 -0500

    chore: add selection tracking

--- src/components/Sudoku/Sudoku.jsx
+++ src/components/Sudoku/Sudoku.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import './sudoku.css'
 
 const Sudoku = () => {
 
-  const Cell = ({ x, y }) => {
+  const [selectedCell, setSelectedCell] = useState([0, 0])
 
-    return <div className="sudoku-cell" data-x={x} data-y={y}></div>
+
+  const selectCell = (x, y) => {
+    setSelectedCell([x, y])
+  }
+
+
+  const Cell = ({ x, y }) => {
+    const [selectedRow, selectedColumn] = selectedCell;
+
+    const className = `sudoku-cell ${(selectedRow === x || selectedColumn === y) && 'selected-section'} ${(selectedRow === x && selectedColumn === y) && 'selected-cell'}`
+
+    return <div className={className} data-x={x} data-y={y} onClick={() => {selectCell(x, y)}}></div>
   }
 
   return <>
@@ -21,92 +33,92 @@ const Sudoku = () => {
         <Cell x={1} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={2} y={1} />
+        <Cell x={2} y={2} />
+        <Cell x={2} y={3} />
+        <Cell x={2} y={4} />
+        <Cell x={2} y={5} />
+        <Cell x={2} y={6} />
+        <Cell x={2} y={7} />
+        <Cell x={2} y={8} />
+        <Cell x={2} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={3} y={1} />
+        <Cell x={3} y={2} />
+        <Cell x={3} y={3} />
+        <Cell x={3} y={4} />
+        <Cell x={3} y={5} />
+        <Cell x={3} y={6} />
+        <Cell x={3} y={7} />
+        <Cell x={3} y={8} />
+        <Cell x={3} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={4} y={1} />
+        <Cell x={4} y={2} />
+        <Cell x={4} y={3} />
+        <Cell x={4} y={4} />
+        <Cell x={4} y={5} />
+        <Cell x={4} y={6} />
+        <Cell x={4} y={7} />
+        <Cell x={4} y={8} />
+        <Cell x={4} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={5} y={1} />
+        <Cell x={5} y={2} />
+        <Cell x={5} y={3} />
+        <Cell x={5} y={4} />
+        <Cell x={5} y={5} />
+        <Cell x={5} y={6} />
+        <Cell x={5} y={7} />
+        <Cell x={5} y={8} />
+        <Cell x={5} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={6} y={1} />
+        <Cell x={6} y={2} />
+        <Cell x={6} y={3} />
+        <Cell x={6} y={4} />
+        <Cell x={6} y={5} />
+        <Cell x={6} y={6} />
+        <Cell x={6} y={7} />
+        <Cell x={6} y={8} />
+        <Cell x={6} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={7} y={1} />
+        <Cell x={7} y={2} />
+        <Cell x={7} y={3} />
+        <Cell x={7} y={4} />
+        <Cell x={7} y={5} />
+        <Cell x={7} y={6} />
+        <Cell x={7} y={7} />
+        <Cell x={7} y={8} />
+        <Cell x={7} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={8} y={1} />
+        <Cell x={8} y={2} />
+        <Cell x={8} y={3} />
+        <Cell x={8} y={4} />
+        <Cell x={8} y={5} />
+        <Cell x={8} y={6} />
+        <Cell x={8} y={7} />
+        <Cell x={8} y={8} />
+        <Cell x={8} y={9} />
       </div>
       <div className="sudoku-row">
-        <Cell x={1} y={1} />
-        <Cell x={1} y={2} />
-        <Cell x={1} y={3} />
-        <Cell x={1} y={4} />
-        <Cell x={1} y={5} />
-        <Cell x={1} y={6} />
-        <Cell x={1} y={7} />
-        <Cell x={1} y={8} />
-        <Cell x={1} y={9} />
+        <Cell x={9} y={1} />
+        <Cell x={9} y={2} />
+        <Cell x={9} y={3} />
+        <Cell x={9} y={4} />
+        <Cell x={9} y={5} />
+        <Cell x={9} y={6} />
+        <Cell x={9} y={7} />
+        <Cell x={9} y={8} />
+        <Cell x={9} y={9} />
       </div>
     </div>
   </>
